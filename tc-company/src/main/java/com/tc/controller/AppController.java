14d89817424376fd040dc2d887c9ae0595db85af
豢涵 <huanhanfu@126.com>
2019-01-15 22:42:11 +0800

    app首页接口完成

---
+++ tc-company/src/main/java/com/tc/controller/AppController.java
@@ -0,0 +1,78 @@
+package com.tc.controller;
+
+import com.tc.db.entity.Task;
+import com.tc.db.entity.TaskClassify;
+import com.tc.db.enums.TaskState;
+import com.tc.dto.app.HomeAppDto;
+import com.tc.dto.app.TaskAppDto;
+import com.tc.dto.app.TaskClassifyAppDto;
+import com.tc.dto.task.QueryTask;
+import com.tc.service.TaskClassifyService;
+import com.tc.service.TaskService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@ResponseStatus(code = HttpStatus.OK)
+@RequestMapping(value = "/app")
+public class AppController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @Autowired
+    private TaskClassifyService taskClassifyService;
+
+
+    @GetMapping("/index")
+    @ApiOperation(value = "获取首页信息")
+    public HomeAppDto index() {
+        HomeAppDto homeAppDto = new HomeAppDto();
+        ArrayList<String> bannsers = new ArrayList<>();
+
+        bannsers.add("https://fuss10.elemecdn.com/7/90/8012dcb83434050f0363316f24809jpeg.jpeg?imageMogr/format/webp/thumbnail/568x/");
+        bannsers.add("https://fuss10.elemecdn.com/d/f5/456ea6285819f7932d8606bffeb35jpeg.jpeg?imageMogr/format/webp/thumbnail/568x/");
+        homeAppDto.setBanners(bannsers);
+
+        //拷贝所有分类
+        List<TaskClassify> taskClassifys = taskClassifyService.findByParents();
+        List<TaskClassifyAppDto> taskClassifyAppDtos = new ArrayList<>();
+        TaskClassifyAppDto taskClassifyAppDto;
+        for (TaskClassify taskClassify : taskClassifys) {
+            taskClassifyAppDto = new TaskClassifyAppDto();
+            BeanUtils.copyProperties(taskClassify, taskClassifyAppDto);
+            taskClassifyAppDtos.add(taskClassifyAppDto);
+        }
+        homeAppDto.setTaskClassifyAppDtos(taskClassifyAppDtos);
+
+
+        //拷贝所有任务
+        List<TaskAppDto> taskAppDtos = new ArrayList<>();
+        QueryTask queryTask = new QueryTask();
+        queryTask.setState(TaskState.ISSUE);
+        List<Task> taskList = taskService.findByQueryTask(queryTask).getContent();
+        TaskAppDto taskAppDto;
+        for (Task task : taskList) {
+            taskAppDto = new TaskAppDto();
+            taskAppDto.setHeadImg(task.getUser().getHeadImg());
+            taskAppDto.setUsername(task.getUser().getUsername());
+            BeanUtils.copyProperties(task, taskAppDto);
+            taskAppDtos.add(taskAppDto);
+        }
+        homeAppDto.setTaskAppDtos(taskAppDtos);
+
+
+        return homeAppDto;
+    }
+
+
+}
---
+++ tc-company/src/main/java/com/tc/dto/app/TaskAppDto.java
@@ -0,0 +1,76 @@
+package com.tc.dto.app;
+
+public class TaskAppDto {
+    private String id;
+    private String name;
+    private String context;
+    private String username;
+    private String headImg;
+    private Float money;
+    private Double longitude;
+    private Double latitude;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getHeadImg() {
+        return headImg;
+    }
+
+    public void setHeadImg(String headImg) {
+        this.headImg = headImg;
+    }
+
+    public Float getMoney() {
+        return money;
+    }
+
+    public void setMoney(Float money) {
+        this.money = money;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+}
